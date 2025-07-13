1bba1b6900d01796bfcf20ef7313ddaa415c47db
Kirill <KARum2004@yandex.ru>
2025-07-13 15:45:14 +0300

    Implement class instead of common function

--- lexer.cpp
+++ lexer.cpp
@@ -1,5 +1,14 @@
 #include "lexer.h"
 
-void lexer(const std::string& expression) {
-    
-}
+class Lexer {
+public:
+    Lexer(const std::string& expression) : expr(expression) {}
+    const std::string& getExpression() const { return expr; }
+    void setExpression(const std::string& expression) { expr = expression; }
+
+    void lexerJob() {
+        
+    }
+private:
+    std::string expr;
+};
--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include "lexer.h"
 #include <string>
+#include <vector>
 
 int main(int argc, char* argv[]) {
     std::string expression;
     std::cin >> expression;
 
-    lexer(expression);
+    const std::vector<std::pair<std::string, std::string>> final_representaion;
+    final_representaion = lexer(expression);
     return 0;
 }
