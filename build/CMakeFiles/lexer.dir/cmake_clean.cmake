80b742643a5e7bc8b295c58f4d9d4df0190bf44c
Kirill <KARum2004@yandex.ru>
2025-07-28 22:16:13 +0300

    Add float parsing

---
+++ build/CMakeFiles/lexer.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lexer.dir/lexer.cpp.o"
+  "CMakeFiles/lexer.dir/lexer.cpp.o.d"
+  "CMakeFiles/lexer.dir/main.cpp.o"
+  "CMakeFiles/lexer.dir/main.cpp.o.d"
+  "lexer"
+  "lexer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lexer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
